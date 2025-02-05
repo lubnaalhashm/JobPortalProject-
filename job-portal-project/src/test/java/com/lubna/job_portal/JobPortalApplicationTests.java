b33ef52ebf2fb82a4c5b3f2ab6c733c71f1b8386
lubnaalhashm <lubnaali7912@gmail.com>
2025-02-05 02:26:16 -0800

    push the project

---
+++ job-portal-project/src/test/java/com/lubna/job_portal/JobPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lubna.job_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
