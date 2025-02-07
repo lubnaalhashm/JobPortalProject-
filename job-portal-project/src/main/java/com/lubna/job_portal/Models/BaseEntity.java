1114fd063d2651b42a23b9675f65a09758e24447
lubnaalhashm <lubnaali7912@gmail.com>
2025-02-06 22:41:39 -0800

    Created a `BaseEntity` class to store `createdAt` and `updatedAt` timestamps.

--- job-portal-project/src/main/java/com/lubna/job_portal/Models/BaseEntity.java
+++ job-portal-project/src/main/java/com/lubna/job_portal/Models/BaseEntity.java
@@ -10,4 +10,10 @@ import java.time.LocalDateTime;
 
     @MappedSuperclass
     public abstract class BaseEntity {
+        @Column(name = "created_at", updatable = false)
+        private LocalDateTime createdAt;
+
+        @Column(name = "updated_at")
+        private LocalDateTime updatedAt;
+
     }
