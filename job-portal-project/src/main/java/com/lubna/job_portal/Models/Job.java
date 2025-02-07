ec827533e1546d53ef0642b6c1038478bd4a8949
lubnaalhashm <lubnaali7912@gmail.com>
2025-02-07 08:20:01 -0800

    pdated Job entity to include job type enum, corrected location column definition, removed ID field (inherited from BaseEntity), and improved validation constraints for salary

--- job-portal-project/src/main/java/com/lubna/job_portal/Models/Job.java
+++ job-portal-project/src/main/java/com/lubna/job_portal/Models/Job.java
@@ -4,15 +4,14 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
+import java.util.List;
 
 @Entity
 @Data
-@Table(name = "job")
-public class JobModel extends BaseEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+@Table(name = "jobs")
+public class Job extends BaseEntity {
 
     @NotBlank(message = "Title is mandatory")
     @Column(nullable = false)
@@ -22,14 +21,19 @@ public class JobModel extends BaseEntity {
     @Column(nullable = false)
     private String description;
 
-    @NotBlank(message = "Location is mandatory ")
-    @Column(nullable = false, columnDefinition = "Varchar(255) defualt 'Remote'")
+    @NotBlank(message = "Location is mandatory")
+    @Column(nullable = false, columnDefinition = "VARCHAR(255) DEFAULT 'Remote'")
     private String location;
 
-    @NotBlank(message = "Salary must be positive")
     @NotNull(message = "Salary cannot be null")
     @Column(nullable = false)
     private Double salary;
 
-}
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private JobType jobType;
 
+    public enum JobType {
+        FULL_TIME, PART_TIME, CONTRACT, INTERNSHIP
+    }
+}
