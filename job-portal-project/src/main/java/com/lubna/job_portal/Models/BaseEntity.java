2da6515bc539a5c994d43e9ff87023fbabbe8d04
lubnaalhashm <lubnaali7912@gmail.com>
2025-02-07 06:01:52 -0800

    Update BaseEntity to include createdAt and updatedAt functionality

--- job-portal-project/src/main/java/com/lubna/job_portal/Models/BaseEntity.java
+++ job-portal-project/src/main/java/com/lubna/job_portal/Models/BaseEntity.java
@@ -1,28 +1,32 @@
 package com.lubna.job_portal.Models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
+import jakarta.persistence.*;
+import lombok.Data;
 
 
 import java.time.LocalDateTime;
 
-    @MappedSuperclass
-    public abstract class BaseEntity {
-        @Column(name = "created_at", updatable = false)
-        private LocalDateTime createdAt;
+@MappedSuperclass
+@Data
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
-        @Column(name = "updated_at")
-        private LocalDateTime updatedAt;
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
 
-        @PrePersist
-        protected void onCreate() {
-            this.createdAt = LocalDateTime.now();
-        }
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 
-        @PreUpdate
-        protected void onUpdate() {
-            this.updatedAt = LocalDateTime.now();
-        }
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
     }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
