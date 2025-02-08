b0c96ef0762348e82266cee68f80708e664e7d8c
lubnaalhashm <lubnaali7912@gmail.com>
2025-02-07 19:55:06 -0800

    Updated JobSeeker entity to include an ID field, renamed phone to phoneNumber, and added a constructor for initializing JobSeeker details.

--- job-portal-project/src/main/java/com/lubna/job_portal/Models/JobSeeker.java
+++ job-portal-project/src/main/java/com/lubna/job_portal/Models/JobSeeker.java
@@ -6,12 +6,27 @@ import lombok.Data;
 @Entity
 @Data
 @Table(name = "job_seekers")
-public class JobSeeker extends BaseEntity{
+public class JobSeeker {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "user_id",referencedColumnName = "id", nullable = false )
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
     private User user;
+
     private String resumeUrl;
     private String portfolioUrl;
-    private String phone;
+    private String phoneNumber;
     private String address;
+
+    public JobSeeker() {}
+
+    public JobSeeker(User user, String resumeUrl, String phoneNumber, String address) {
+        this.user = user;
+        this.resumeUrl = resumeUrl;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
 }
