1fe6807a57b50b342f3fc66c3540649f4d570510
lubnaalhashm <lubnaali7912@gmail.com>
2025-02-07 21:00:15 -0800

    Updated UserRepository to include a method for checking if an active user exists by email

--- job-portal-project/src/main/java/com/lubna/job_portal/Repositories/UserRepository.java
+++ job-portal-project/src/main/java/com/lubna/job_portal/Repositories/UserRepository.java
@@ -4,12 +4,16 @@ import com.lubna.job_portal.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
+    Optional<User> findByEmail(String email);
+
     @Query("SELECT u from User u where u.email = :email and u.isActive = true")
-    Optional<User> findByEmail(@Param("email") String email);
+    Optional<User> findActiveUserByEmail(@Param("email") String email);
 
     @Query("SELECT count(u) > 0 from User u where u.email = :email and u.isActive = true")
     boolean existsByEmail(@Param("email") String email);
