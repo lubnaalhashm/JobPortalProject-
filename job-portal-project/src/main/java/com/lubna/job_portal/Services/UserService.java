a35f3457ce224f8436ddbcd2dc8e5cf432e77747
lubnaalhashm <lubnaali7912@gmail.com>
2025-02-09 06:52:27 -0800

    Create UserService class to handle user-related operations using UserRepositor

---
+++ job-portal-project/src/main/java/com/lubna/job_portal/Services/UserService.java
@@ -0,0 +1,11 @@
+package com.lubna.job_portal.Services;
+
+import com.lubna.job_portal.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+}
