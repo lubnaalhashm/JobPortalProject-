078ccf0a11dbe60ca659b3b9592159891dded631
lubnaalhashm <lubnaali7912@gmail.com>
2025-02-08 21:45:20 -0800

    Updated JobSeekerDTO to include portfolioUrl, phoneNumber, and address fields along with their getter and setter methods.

--- job-portal-project/src/main/java/com/lubna/job_portal/DTOs/JobSeekerDTO.java
+++ job-portal-project/src/main/java/com/lubna/job_portal/DTOs/JobSeekerDTO.java
@@ -7,4 +7,63 @@ public class JobSeekerDTO {
     private String name;
     private String email;
     private String resumeUrl;
+    private String portfolioUrl;
+    private String phoneNumber;
+    private String address;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getResumeUrl() {
+        return resumeUrl;
+    }
+
+    public void setResumeUrl(String resumeUrl) {
+        this.resumeUrl = resumeUrl;
+    }
+
+    public String getPortfolioUrl() {
+        return portfolioUrl;
+    }
+
+    public void setPortfolioUrl(String portfolioUrl) {
+        this.portfolioUrl = portfolioUrl;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
