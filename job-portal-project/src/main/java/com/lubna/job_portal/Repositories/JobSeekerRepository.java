0088eba9c9ed320938408a03351af822db77293d
lubnaalhashm <lubnaali7912@gmail.com>
2025-02-09 05:59:22 -0800

    Add method to find job seeker by phone number using a custom query

--- job-portal-project/src/main/java/com/lubna/job_portal/Repositories/JobSeekerRepository.java
+++ job-portal-project/src/main/java/com/lubna/job_portal/Repositories/JobSeekerRepository.java
@@ -2,6 +2,7 @@ package com.lubna.job_portal.Repositories;
 
 import com.lubna.job_portal.Models.JobSeeker;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -13,4 +14,8 @@ public interface JobSeekerRepository extends JpaRepository<JobSeeker, Integer> {
 
     // Find a job Seeker by their associated user ID
     Optional<JobSeeker> findByUserId(Integer userId);
+
+    // Custom query to find job Seekers by phone number
+    @Query("SELECT js from JobSeeker js where js.phoneNumber = :phoneNumber")
+    Optional<JobSeeker> findByPhoneNumber(String phoneNumber);
 }
