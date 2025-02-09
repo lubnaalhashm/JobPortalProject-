5e39e2c0f9b95352accd1872808af898a1dc4e51
lubnaalhashm <lubnaali7912@gmail.com>
2025-02-08 21:55:53 -0800

    Add JobApplicationDTO with fields for id, jobId, jobSeekerId, status, cover letter, and appliedAt along with getter and setter methods

---
+++ job-portal-project/src/main/java/com/lubna/job_portal/DTOs/JobApplicationDTO.java
@@ -0,0 +1,60 @@
+package com.lubna.job_portal.DTOs;
+
+import java.time.LocalDateTime;
+
+public class JobApplicationDTO {
+    private Integer id;
+    private Integer jobId;
+    private  Integer jobSeekerId;
+    private String status;
+    private String coverletter;
+    private LocalDateTime appliedAt;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getJobId() {
+        return jobId;
+    }
+
+    public void setJobId(Integer jobId) {
+        this.jobId = jobId;
+    }
+
+    public Integer getJobSeekerId() {
+        return jobSeekerId;
+    }
+
+    public void setJobSeekerId(Integer jobSeekerId) {
+        this.jobSeekerId = jobSeekerId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getCoverletter() {
+        return coverletter;
+    }
+
+    public void setCoverletter(String coverletter) {
+        this.coverletter = coverletter;
+    }
+
+    public LocalDateTime getAppliedAt() {
+        return appliedAt;
+    }
+
+    public void setAppliedAt(LocalDateTime appliedAt) {
+        this.appliedAt = appliedAt;
+    }
+}
