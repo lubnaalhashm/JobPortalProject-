af26f9cc053ed9f50285244fa662e1d53bae7a31
lubnaalhashm <lubnaali7912@gmail.com>
2025-02-08 23:41:51 -0800

    Add methods in JobDTO to convert between Job and JobDTO, including single object and list conversions

--- job-portal-project/src/main/java/com/lubna/job_portal/DTOs/JobDTO.java
+++ job-portal-project/src/main/java/com/lubna/job_portal/DTOs/JobDTO.java
@@ -1,42 +1,59 @@
 package com.lubna.job_portal.DTOs;
 
-import jakarta.persistence.criteria.CriteriaBuilder;
+import com.lubna.job_portal.Models.Job;
+import lombok.Data;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
 public class JobDTO {
     private Integer id;
     private String title;
     private String description;
     private String location;
+    private Double salary;
 
-    public Integer getId() {
-        return id;
+
+    public static JobDTO convertToDTO(Job job) {
+        JobDTO jobDto = new JobDTO();
+        if (job != null) {
+            jobDto.setId(job.getId());
+            jobDto.setTitle(job.getTitle());
+            jobDto.setDescription(job.getDescription());
+            jobDto.setLocation(job.getLocation());
+            jobDto.setSalary(job.getSalary());
+        }
+        return jobDto;
     }
-
-    public void setId(Integer id) {
-        this.id = id;
+    public static List<JobDTO> convertToDTO(List<Job> jobList) {
+        List<JobDTO> jobDtoList = new ArrayList<>();
+        if (jobList != null && !jobList.isEmpty()) {
+            for (Job job : jobList) {
+                jobDtoList.add(convertToDTO(job));
+            }
+        }
+        return jobDtoList;
     }
-
-    public String getTitle() {
-        return title;
+    public static Job convertFromDTO(JobDTO jobDto) {
+        Job job = new Job();
+        if (jobDto != null) {
+            job.setId(jobDto.getId());
+            job.setTitle(jobDto.getTitle());
+            job.setDescription(jobDto.getDescription());
+            job.setLocation(jobDto.getLocation());
+            job.setSalary(job.getSalary());
+        }
+        return job;
     }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
+    public static List<Job> convertFromDTO(List<JobDTO> jobDtoList) {
+        List<Job> jobList = new ArrayList<>();
+        if (jobDtoList != null && !jobDtoList.isEmpty()) {
+            for (JobDTO dto : jobDtoList) {
+                jobList.add(convertFromDTO(dto));
+            }
+        }
+        return jobList;
     }
 }
