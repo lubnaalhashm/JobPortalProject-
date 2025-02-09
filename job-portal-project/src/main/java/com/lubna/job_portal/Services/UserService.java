3a9020baf67eaef6439e9ec7de48db5c0b4de6b5
lubnaalhashm <lubnaali7912@gmail.com>
2025-02-09 07:31:17 -0800

    Added method to retrieve a user by ID and convert it to UserDTO in UserService

--- job-portal-project/src/main/java/com/lubna/job_portal/Services/UserService.java
+++ job-portal-project/src/main/java/com/lubna/job_portal/Services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -17,8 +18,13 @@ public class UserService {
     //Get all users
     public List<UserDTO> getAllUser() {
         List<User> users = userRepository.findAll();
-
         return UserDTO.convertToDTO(users);
     }
+
+    public UserDTO getUserById(Integer id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        return optionalUser.map(UserDTO::convertToDTO).orElse(new UserDTO());
+    }
+
 }
 
