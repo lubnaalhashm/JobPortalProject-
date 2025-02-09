d540dc3520eb79b18ae101bb38e2065df0546a72
lubnaalhashm <lubnaali7912@gmail.com>
2025-02-09 07:04:28 -0800

    Create method to retrieve all users and convert them to UserDTO in UserService

--- job-portal-project/src/main/java/com/lubna/job_portal/Services/UserService.java
+++ job-portal-project/src/main/java/com/lubna/job_portal/Services/UserService.java
@@ -1,11 +1,24 @@
 package com.lubna.job_portal.Services;
 
+import com.lubna.job_portal.DTOs.UserDTO;
+import com.lubna.job_portal.Models.User;
 import com.lubna.job_portal.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+
+    //Get all users
+    public List<UserDTO> getAllUser() {
+        List<User> users = userRepository.findAll();
+
+        return UserDTO.convertToDTO(users);
+    }
 }
+
