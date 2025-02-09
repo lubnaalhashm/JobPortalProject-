8d24b79d3d1702944aedfdfd3ecd7c3220b1d16e
lubnaalhashm <lubnaali7912@gmail.com>
2025-02-08 21:36:41 -0800

    Add UserDTO class with fields for id, username, email, and password along with getter and setter methods

---
+++ job-portal-project/src/main/java/com/lubna/job_portal/DTOs/UserDTO.java
@@ -0,0 +1,42 @@
+package com.lubna.job_portal.DTOs;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+
+public class UserDTO {
+    private Integer id;
+    private String username;
+    private String email;
+    private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
