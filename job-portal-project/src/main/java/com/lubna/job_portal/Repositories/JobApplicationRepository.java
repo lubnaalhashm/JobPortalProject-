e5b59ba57705239154a0847d3ffce9c17b4b6d89
lubnaalhashm <lubnaali7912@gmail.com>
2025-02-08 20:35:57 -0800

    Add method to check if a job seeker has applied to a specific job in JobSeekerRepository.

--- job-portal-project/src/main/java/com/lubna/job_portal/Repositories/JobApplicationRepository.java
+++ job-portal-project/src/main/java/com/lubna/job_portal/Repositories/JobApplicationRepository.java
@@ -4,13 +4,17 @@ import com.lubna.job_portal.Models.JobApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
-public interface JobSeekerRepository extends JpaRepository<JobApplication, Integer> {
+@Repository
+public interface JobApplicationRepository extends JpaRepository<JobApplication, Integer> {
     @Query("SELECT ja from JobApplication ja where ja.jobSeeker.id = :jobSeekerId order by ja.appliedAt DESC")
     List<JobApplication> findByJobSeekerId(@Param("jobSeekerId") Integer jobSeekerId);
 
     @Query("SELECT ja from JobApplication ja where ja.job.id = :jobId order by ja.appliedAt DESC")
-    List<JobApplication> findByJobId(@Param("jobId") Long jobId);
+    List<JobApplication> findByJobId(@Param("jobId") Integer jobId);
+
+    @Query("SELECT count(ja) > 0 from JobApplication ja where ja.job.id = :jobId and ja.jobSeeker.id = :jobSeekerId")
+    boolean hasApplied(@Param("jobId") Integer jobId, @Param("jobSeekerId") Integer jobSeekerId);
 }
