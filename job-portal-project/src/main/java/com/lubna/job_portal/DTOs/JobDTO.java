5668eefa92f2cae51a62694068dc8d51a63245b7
lubnaalhashm <lubnaali7912@gmail.com>
2025-02-08 21:35:30 -0800

    Added JobDTO class with fields for id, title, description, and location along with getter and setter methods

---
+++ job-portal-project/src/main/java/com/lubna/job_portal/DTOs/JobDTO.java
@@ -0,0 +1,42 @@
+package com.lubna.job_portal.DTOs;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+
+public class JobDTO {
+    private Integer id;
+    private String title;
+    private String description;
+    private String location;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
