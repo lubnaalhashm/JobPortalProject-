628828477fb6cf745c92d7df29d618650bd423ba
lubnaalhashm <lubnaali7912@gmail.com>
2025-02-09 05:54:00 -0800

    Add method to check if a job seeker exists by ID in JobSeekerRepository

--- job-portal-project/src/main/java/com/lubna/job_portal/Repositories/JobSeekerRepository.java
+++ job-portal-project/src/main/java/com/lubna/job_portal/Repositories/JobSeekerRepository.java
@@ -4,7 +4,11 @@ import com.lubna.job_portal.Models.JobSeeker;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface JobSeekerRepository extends JpaRepository<JobSeeker, Integer> {
+    //Check if a job Seeker exists by ID
+    boolean existsById(Integer id);
     
 }
