81d5b77f9441aa1f04713289b1a49171c3d6c8b7
lubnaalhashm <lubnaali7912@gmail.com>
2025-02-08 21:43:22 -0800

    Add JobSeekerDTO class with fields for id, name, email, and resumeUrl."   Ask  Explain

---
+++ job-portal-project/src/main/java/com/lubna/job_portal/DTOs/JobSeekerDTO.java
@@ -0,0 +1,10 @@
+package com.lubna.job_portal.DTOs;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+
+public class JobSeekerDTO {
+    private Integer id;
+    private String name;
+    private String email;
+    private String resumeUrl;
+}
