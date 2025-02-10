8583111d6c90c6dd8d2455a971e24980ebe0ad6e
lubnaalhashm <lubnaali7912@gmail.com>
2025-02-10 05:22:28 -0800

    add method and query to check if a job exists by its ID

--- job-portal-project/src/main/java/com/lubna/job_portal/Repositories/JobRepository.java
+++ job-portal-project/src/main/java/com/lubna/job_portal/Repositories/JobRepository.java
@@ -5,8 +5,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface JobRepository extends JpaRepository<Job, Integer> {
     @Query(value = "SELECT count(j) > 0 from Job j where j.id = :jobId AND j.expiresAt > current_timestamp", nativeQuery = true)
     boolean isJobActive(@Param("jobId") Integer jobId);
 
+    // check if a job exists by ID
+    boolean existsById(Integer id);
+
+    // query to find active jobs
+    @Query("SELECT j FROM Job j WHERE j.isActive = true")
+    List<Job> findByIsActiveTrue();
 }
