dc7293a771fb03aceca54c132783aaccef024779
lubnaalhashm <lubnaali7912@gmail.com>
2025-02-10 21:55:15 -0800

    Add methods to create and check job seekers

--- job-portal-project/src/main/java/com/lubna/job_portal/Services/JobSeekerService.java
+++ job-portal-project/src/main/java/com/lubna/job_portal/Services/JobSeekerService.java
@@ -3,6 +3,7 @@ package com.lubna.job_portal.Services;
 import com.lubna.job_portal.DTOs.JobSeekerDTO;
 import com.lubna.job_portal.Models.JobSeeker;
 import com.lubna.job_portal.Repositories.JobSeekerRepository;
+import com.lubna.job_portal.Utils.HelperUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,4 +18,27 @@ public class JobSeekerService {
         List<JobSeeker> jobSeekers = jobSeekerRepository.findAll();
         return JobSeekerDTO.convertToDTO(jobSeekers);
     }
+    // Get Job Seeker by ID
+   /* public JobSeekerDTO getJobSeekerById(Integer id) {
+        JobSeeker entity = jobSeekerRepository.getJobSeekerById(id);
+        return JobSeekerDTO.convertToDTO(entity);*/
+
+    public JobSeekerDTO addJobSeeker(JobSeekerDTO jobSeekerDto) {
+        if (HelperUtils.isNull(jobSeekerDto.getId()) || !checkIfJobSeekerExists(jobSeekerDto.getId())) {
+            JobSeeker jobSeeker = JobSeekerDTO.convertFromDTO(jobSeekerDto);
+            jobSeeker = jobSeekerRepository.save(jobSeeker);
+            return JobSeekerDTO.convertToDTO(jobSeeker);
+        }
+        return new JobSeekerDTO();
+    }
+
+
+
+
+
+    public boolean checkIfJobSeekerExists(Integer id) {
+        return jobSeekerRepository.existsById(id);
+    }
 }
+
+
