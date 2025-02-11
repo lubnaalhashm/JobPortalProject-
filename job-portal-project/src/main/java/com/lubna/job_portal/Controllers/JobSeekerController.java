75430361f866ce05a8da3675a1c1a3010eb30019
lubnaalhashm <lubnaali7912@gmail.com>
2025-02-10 20:42:42 -0800

    Add method to fetch all job seekers

--- job-portal-project/src/main/java/com/lubna/job_portal/Controllers/JobSeekerController.java
+++ job-portal-project/src/main/java/com/lubna/job_portal/Controllers/JobSeekerController.java
@@ -1,4 +1,22 @@
 package com.lubna.job_portal.Controllers;
 
+import com.lubna.job_portal.DTOs.JobSeekerDTO;
+import com.lubna.job_portal.Models.JobSeeker;
+import com.lubna.job_portal.Repositories.JobSeekerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class JobSeekerController {
+
+    @Autowired
+    private JobSeekerRepository jobSeekerRepository;
+
+    public List<JobSeekerDTO> getAllJobSeekers() {
+        List<JobSeeker> jobSeekers = jobSeekerRepository.findAll();
+        return JobSeekerDTO.convertToDTO(jobSeekers);
+    }
+
 }
