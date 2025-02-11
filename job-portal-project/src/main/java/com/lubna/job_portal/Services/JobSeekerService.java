43f633892334c01af84532a77caa9e7aaea0f4b8
lubnaalhashm <lubnaali7912@gmail.com>
2025-02-10 21:45:15 -0800

    Created a new `JobSeekerService` class annotated with `@Service`

--- job-portal-project/src/main/java/com/lubna/job_portal/Services/JobSeekerService.java
+++ job-portal-project/src/main/java/com/lubna/job_portal/Services/JobSeekerService.java
@@ -1,5 +1,11 @@
 package com.lubna.job_portal.Services;
 
+import com.lubna.job_portal.Models.JobSeeker;
+import com.lubna.job_portal.Repositories.JobSeekerRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class JobSeekerService {
 
+    private JobSeekerRepository jobSeekerRepository;
 }
