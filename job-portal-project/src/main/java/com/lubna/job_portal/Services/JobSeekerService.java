9b942020444a02837ad60f4caac01c4413acc7c5
lubnaalhashm <lubnaali7912@gmail.com>
2025-02-10 21:47:32 -0800

    Create method to get all job seekers

--- job-portal-project/src/main/java/com/lubna/job_portal/Services/JobSeekerService.java
+++ job-portal-project/src/main/java/com/lubna/job_portal/Services/JobSeekerService.java
@@ -1,11 +1,20 @@
 package com.lubna.job_portal.Services;
 
+import com.lubna.job_portal.DTOs.JobSeekerDTO;
 import com.lubna.job_portal.Models.JobSeeker;
 import com.lubna.job_portal.Repositories.JobSeekerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class JobSeekerService {
-
+    @Autowired
     private JobSeekerRepository jobSeekerRepository;
+
+    public List<JobSeekerDTO> getAllJobSeekers() {
+        List<JobSeeker> jobSeekers = jobSeekerRepository.findAll();
+        return JobSeekerDTO.convertToDTO(jobSeekers);
+    }
 }
