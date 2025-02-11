80176f9ffb197bf39b7e4f8abfbe5ef26397890f
lubnaalhashm <lubnaali7912@gmail.com>
2025-02-10 20:48:50 -0800

    Add UserRepository to JobSeekerController

--- job-portal-project/src/main/java/com/lubna/job_portal/Controllers/JobSeekerController.java
+++ job-portal-project/src/main/java/com/lubna/job_portal/Controllers/JobSeekerController.java
@@ -3,6 +3,7 @@ package com.lubna.job_portal.Controllers;
 import com.lubna.job_portal.DTOs.JobSeekerDTO;
 import com.lubna.job_portal.Models.JobSeeker;
 import com.lubna.job_portal.Repositories.JobSeekerRepository;
+import com.lubna.job_portal.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +14,13 @@ public class JobSeekerController {
 
     @Autowired
     private JobSeekerRepository jobSeekerRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     public List<JobSeekerDTO> getAllJobSeekers() {
         List<JobSeeker> jobSeekers = jobSeekerRepository.findAll();
         return JobSeekerDTO.convertToDTO(jobSeekers);
     }
+
 
 }
