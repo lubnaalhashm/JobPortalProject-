086f6e52d435fa7d07dba572c24c7eed3215c09c
lubnaalhashm <lubnaali7912@gmail.com>
2025-02-10 23:00:07 -0800

    Create JobSeekerController with base URL mapping

--- job-portal-project/src/main/java/com/lubna/job_portal/Controllers/JobSeekerController.java
+++ job-portal-project/src/main/java/com/lubna/job_portal/Controllers/JobSeekerController.java
@@ -1,26 +1,14 @@
 package com.lubna.job_portal.Controllers;
 
-import com.lubna.job_portal.DTOs.JobSeekerDTO;
-import com.lubna.job_portal.Models.JobSeeker;
-import com.lubna.job_portal.Repositories.JobSeekerRepository;
-import com.lubna.job_portal.Repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
-@Service
+@RestController
+@RequestMapping(value = "job-Seeker")
 public class JobSeekerController {
-
-    @Autowired
-    private JobSeekerRepository jobSeekerRepository;
-    @Autowired
-    private UserRepository userRepository;
-
-    public List<JobSeekerDTO> getAllJobSeekers() {
-        List<JobSeeker> jobSeekers = jobSeekerRepository.findAll();
-        return JobSeekerDTO.convertToDTO(jobSeekers);
-    }
-
+    private final Logger logger = LoggerFactory.getLogger(JobSeekerController.class);
 
 }
