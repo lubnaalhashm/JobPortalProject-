264d53e062e86e80335fe22b58c10c538041ede7
lubnaalhashm <lubnaali7912@gmail.com>
2025-02-10 23:04:13 -0800

    Add endpoint to fetch all job seekers with error handling

--- job-portal-project/src/main/java/com/lubna/job_portal/Controllers/JobSeekerController.java
+++ job-portal-project/src/main/java/com/lubna/job_portal/Controllers/JobSeekerController.java
@@ -1,11 +1,16 @@
 package com.lubna.job_portal.Controllers;
 
+import com.lubna.job_portal.DTOs.JobSeekerDTO;
 import com.lubna.job_portal.Services.JobSeekerService;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RestController
@@ -15,5 +20,16 @@ public class JobSeekerController {
 
     @Autowired
     private JobSeekerService jobSeekerService;
+
+    @GetMapping(value = "getAll")
+    public List<JobSeekerDTO> getAllJobSeekers() {
+        List<JobSeekerDTO> jobSeekerDTOList = new ArrayList<>();
+        try {
+            jobSeekerDTOList.addAll(jobSeekerService.getAllJobSeekers());
+        } catch (Exception e) {
+            logger.error("Error while fetching all job seekers: {}", e.getMessage());
+        }
+        return jobSeekerDTOList;
+    }
 
 }
