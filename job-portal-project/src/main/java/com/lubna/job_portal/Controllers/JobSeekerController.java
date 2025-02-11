81da2ca8e82676bb62887b0d97aa182ca7e66db1
lubnaalhashm <lubnaali7912@gmail.com>
2025-02-10 23:02:10 -0800

    Added `JobSeekerService` as a dependency in the `JobSeekerController` using `@Autowired`.

--- job-portal-project/src/main/java/com/lubna/job_portal/Controllers/JobSeekerController.java
+++ job-portal-project/src/main/java/com/lubna/job_portal/Controllers/JobSeekerController.java
@@ -1,7 +1,9 @@
 package com.lubna.job_portal.Controllers;
 
+import com.lubna.job_portal.Services.JobSeekerService;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "job-Seeker")
 public class JobSeekerController {
     private final Logger logger = LoggerFactory.getLogger(JobSeekerController.class);
+
+    @Autowired
+    private JobSeekerService jobSeekerService;
 
 }
