c7b533b86d74c234258e6cd1bfb80c072e8a2fee
lubnaalhashm <lubnaali7912@gmail.com>
2025-02-15 23:38:30 -0800

    Add @CrossOrigin annotation to allow cross-origin requests

--- job-portal-project/src/main/java/com/lubna/job_portal/Controllers/UserController.java
+++ job-portal-project/src/main/java/com/lubna/job_portal/Controllers/UserController.java
@@ -15,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "user")
+@CrossOrigin(origins ="*")
 public class UserController {
     private final Logger logger = LoggerFactory.getLogger(UserController.class);
 
