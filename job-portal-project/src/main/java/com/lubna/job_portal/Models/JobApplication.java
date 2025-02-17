405e27f6c12b6aa806e3892ee4d8b5ea7555058f
lubnaalhashm <lubnaali7912@gmail.com>
2025-02-17 02:54:57 -0800

    Rename the job seeker to applicant

--- job-portal-project/src/main/java/com/lubna/job_portal/Models/JobApplication.java
+++ job-portal-project/src/main/java/com/lubna/job_portal/Models/JobApplication.java
@@ -11,6 +11,7 @@ import java.time.LocalDateTime;
 public class JobApplication extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "job_seeker_id", nullable = false)
+    private JobSeeker applicant;
 
     @ManyToOne
     @JoinColumn(name = "job_id", nullable = false)
