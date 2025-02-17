1042facaa627920ab2a12be46d5e5cf34024d1fd
lubnaalhashm <lubnaali7912@gmail.com>
2025-02-17 02:22:53 -0800

    Added JobApplicationController with base request mapping

--- job-portal-project/src/main/java/com/lubna/job_portal/Controllers/JobApplicationController.java
+++ job-portal-project/src/main/java/com/lubna/job_portal/Controllers/JobApplicationController.java
@@ -1,1 +1,21 @@
+package com.lubna.job_portal.Controllers;
+
+
+import com.lubna.job_portal.DTOs.JobApplicationDTO;
+import com.lubna.job_portal.Enum.JobType;
+import com.lubna.job_portal.Services.JobApplicationsService;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 @RestController
+@RequestMapping(value = "jobApplication")
+public class JobApplicationController {
+
+
+}
