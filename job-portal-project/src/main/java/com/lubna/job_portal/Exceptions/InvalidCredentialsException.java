80bc67f4285159b75ef1edb4e03741905631eada
lubnaalhashm <lubnaali7912@gmail.com>
2025-02-22 21:58:25 -0800

    Added InvalidCredentialsException for handling authentication failures

---
+++ job-portal-project/src/main/java/com/lubna/job_portal/Exceptions/InvalidCredentialsException.java
@@ -0,0 +1,7 @@
+package com.lubna.job_portal.Exceptions;
+
+public class InvalidCredentialsException extends RuntimeException{
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+}
