8272b0a7090dba6e7491cbd9d9408b4a85ba4e1f
lubnaalhashm <lubnaali7912@gmail.com>
2025-02-22 21:53:31 -0800

    Create Security Config class

---
+++ job-portal-project/src/main/java/com/lubna/job_portal/Services/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.lubna.job_portal.Services;
+
+
+import com.lubna.job_portal.Models.User;
+import com.lubna.job_portal.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole().name()) // Ensure role handling
+                .build();
+    }
+}
